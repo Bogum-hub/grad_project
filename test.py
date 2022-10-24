7dc7973fee6724b5e7e92ccc1d3ad83e5336234d
John Doe <s1020318@gmail.com>
2022-10-24 12:05:22 +0800

    1024

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("http://localhost:5000/pred", files={'file':open('photo/bet.jpg', 'rb')})
+resp = requests.post("https://dont-drug-out.herokuapp.com/pred", files={'file':open('photo/bet.jpg', 'rb')})
 
 print(resp.text)
